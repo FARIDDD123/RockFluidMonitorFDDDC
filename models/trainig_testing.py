d94528eb3e1ef42a5410d4943214c580d49c11b3
Abolfazle dehbozorgi <alic25119@gmail.com>
2025-06-05 20:48:02 +0330

    Update trainig_testing.py

--- models/trainig_testing.py
+++ models/trainig_testing.py
@@ -8,33 +8,25 @@ import torch.optim as optim
 from sklearn.model_selection import TimeSeriesSplit
 from sklearn.metrics import classification_report
 from model import min_max_scale_3d
-import torch.nn.utils.prune as prune
+import  torch.nn.utils.prune as prune
 from torch.utils.data import TensorDataset, DataLoader
-import random 
+import gc
+from model import create_sequences
 import matplotlib.pyplot as plt 
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import roc_curve
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-import gc
-df = pd.read_parquet('your_data.parquet')
-col = ['Emulsion_Risk'                  
-,'Oil_Water_Ratio'                
-,'Solid_Content_%'                
-,'Formation_Damage_Index'         
-,'LONG'                           
-,'LAT'                            
-,'Mud_Weight_ppg'                 
-,'Fracture_Gradient_ppg'          
-,'Depth_m'
-,'pH_Level'
-,'Plastic_Viscosity'
-,'Viscosity_cP']
-
-df_for_lstm = df[col]
+df = pd.read_parquet('FDMS_well_WELL_1.parquet',engine='fastparquet')
+col = ['Oil_Water_Ratio'
+       ,'Emulsion_Stability'
+       ,'Formation_Damage_Index'
+       ,'Emulsion_Risk',
+       'timestamp']
+df = df[col]
 
 def make_binary(df,threshold=0.5):
     df['Emulsion_Risk_binary'] = np.where(df['Emulsion_Risk'] > threshold,1,0)
-    return df
+    return df.drop(columns=['Emulsion_Risk_binary']),df['Emulsion_Risk_binary']
 
 def prune_model(model,amount=0.2):
     for name,_ in model.named_modules():
@@ -42,67 +34,48 @@ def prune_model(model,amount=0.2):
             prune.l1_unstructured(name,name='weight',amount=amount)
             prune.remove(name,name='weight')
         except:
-            continue 
+            continue
+def future_eng(df):
+  df.set_index(['timestamp'],inplace=True)
+  er = df['Emulsion_Risk']
+  df.drop(columns=['Emulsion_Risk'],inplace=True)
+  for col1 in df.columns:
+    df[f'{col1}_dy_dx'] = np.gradient(df[col1])  
+    df[f'{col1}_std_10s'] = df[f'{col1}'].groupby(pd.Grouper(freq='2s')).std()
+    df[f'{col1}_mean_10s'] = df[f'{col1}'].groupby(pd.Grouper(freq='2s')).mean()
+    df[f'{col1}_tanh'] = np.tanh(df[col1])
+    df[f'{col1}/Emulsion_Risk_shift_1s'] = df[col1] / er.shift(1)
+    df[f'{col1}_ma'] = df[col1].rolling(window=20).mean()
+    df[f'{col1}_ema'] = df[col1].ewm(span=20,adjust=False).mean()
+    df[f"{col1}_diff"] = df[col1].diff()
+    df[f"{col1}_to_ES_ratio"] = df[col1].shift(1) / (df["Emulsion_Stability"].shift(1) + 1e-6)
+  df["OWR_rolling_std_10"] = df["Oil_Water_Ratio"].rolling(window=10).std()
+  df['Emulsion_Risk_shift_1s'] = er.shift(1)
+  return df
 
-def balanced_time_series_windows(X, y, window_size=10, threshold=0.5, seed=42):
-    np.random.seed(seed)
-    random.seed(seed)
-    
-    pos_windows = []
-    neg_windows = []
-
-    for start in range(0, len(X) - window_size + 1):
-        end = start + window_size
-        x_win = X.iloc[start:end].values
-        y_win = y.iloc[start:end].values
-
-        pos_ratio = np.mean(y_win)
-
-        if pos_ratio >= threshold:
-            pos_windows.append((x_win, 1))
-        elif pos_ratio <= (1 - threshold):
-            neg_windows.append((x_win, 0))
-        gc.collect()
-
-    num_pos = len(pos_windows)
-    num_neg = len(neg_windows)
-
-    if num_neg == 0 or num_pos == 0:
-        raise ValueError("No valid windows found for one of the classes. Try adjusting window_size or threshold.")
-
-    print(f"Positive windows: {num_pos} | Negative windows: {num_neg}")
-
-    # smarter sampling
-    neg_sampled = neg_windows
-    pos_sampled = random.sample(pos_windows, k=len(neg_sampled))
-    gc.collect()
-    combined = pos_sampled + neg_sampled
-    random.shuffle(combined)
-
-    x = np.array([item[0] for item in combined])
-    y = np.array([item[1] for item in combined])
-
-    return x, y
-
+    x,y = make_binary(df)
+df_for_lstm = future_eng(x)
+df_for_lstm.reset_index(inplace=True)
+x.drop(columns=['timestamp'],inplace=True)
 df_for_lstm = make_binary(df_for_lstm)
 
-X,y = balanced_time_series_windows(X=df_for_lstm.drop(columns=['Emulsion_Risk_binary','Emulsion_Risk']),y=df_for_lstm['Emulsion_Risk_binary'])
-
-X = np.nan_to_num(X)
-
+gc.collect()
+X,y = df_for_lstm.iloc[:20_000],y.iloc[:20_000]
+X.fillna(0,inplace=True)
 tss = TimeSeriesSplit(n_splits=6)
 
+X,y = create_sequences(data=X,targets=y,lookback=20)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-model = LSTM_with_Attention(input_dim=X.shape[2],hidden_dim=64,output_dim=2,dropout_rate=0.4,lstm_layers=2).to(device=device) 
+model = LSTM_with_Attention(input_dim=X.shape[2],hidden_dim=128,output_dim=2,dropout_rate=0.3,lstm_layers=2).to(device=device) 
 
 
 
 
-optims = optim.Adam(model.parameters(),lr=0.0002)
-lr_scheduler = CosineAnnealingWarmRestarts(optimizer=optims,T_0=1,T_mult=2)
-loss_fn = nn.CrossEntropyLoss().to(device=device)
+optims = optim.Adam(model.parameters(), lr=0.001)
+lr_scheduler = CosineAnnealingWarmRestarts(optimizer=optims,T_0=3,T_mult=2)
+loss_fn = nn.CrossEntropyLoss().to(device=device) 
 
 BATCH_SIZE = 100
 EPOCHS = 100 
@@ -142,8 +115,6 @@ for epoch in range(EPOCHS):
         model.eval()
 
         with torch.inference_mode():
-
-            
             for batch_x_test, batch_y_test in test_loader:
                 y_pred_test = model(batch_x_test.to(device)).squeeze(1)
                 loss_test_batch = loss_fn(y_pred_test,batch_y_test.to(device=device))
@@ -155,21 +126,26 @@ for epoch in range(EPOCHS):
 
 
 
-df2 = pd.read_parquet('your_data.parquet')
+df2 = pd.read_parquet('FDMS_well_WELL_2.parquet',engine='fastparquet')
+df2 = df2.iloc[:10_000]
 
 df_for_lstm2 = df2[col]
-df_for_lstm2 = make_binary(df_for_lstm2)
+X2,y2 = make_binary(df_for_lstm2)
 
-X2,y2 = balanced_time_series_windows(X=df_for_lstm2.drop(columns=['Emulsion_Risk_binary','Emulsion_Risk']),y=df_for_lstm2['Emulsion_Risk_binary'])
-
+X2 = future_eng(X2)
+X2.reset_index(inplace=True)
+X2.drop(columns=['timestamp'],inplace=True)
+X2,y2 = create_sequences(data=X2,targets=y2,lookback=20)
 X2 = np.nan_to_num(X2)
 x_test_scaled2,_ = min_max_scale_3d(X2,X2)
 
+
+
 x_test_tensor2 = torch.FloatTensor(x_test_scaled2)
-y_test_tensor2 = torch.tensor(y2)
+y_test_tensor2 = torch.tensor(y,dtype=torch.long)
 
 with torch.inference_mode():
-        y_pred_test2 = model(x_test_tensor2[:100].to(device)).squeeze(1)
+        y_pred_test2 = model(x_test_tensor2[:128].to(device)).squeeze(1)
 
 
 print(f"\nclassification_report:{classification_report(y_true=y_test_tensor2[:len(y_pred_test2)],y_pred=y_pred_test2.argmax(dim=1).cpu().numpy(),zero_division=0)}")
